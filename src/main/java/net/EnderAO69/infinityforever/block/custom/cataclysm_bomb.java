8248cf9e8dafaacd395f40f9bf4051052b6e40ab
EnderAO69 <alexanderotoole@icloud.com>
2022-06-21 19:03:58 +0100

    Commit

---
+++ src/main/java/net/EnderAO69/infinityforever/block/custom/cataclysm_bomb.java
@@ -0,0 +1,10 @@
+package net.EnderAO69.infinityforever.block.custom;
+
+import net.minecraft.block.Block;
+
+public class cataclysm_bomb extends Block {
+    public cataclysm_bomb(Settings settings) {
+        super(settings);
+    }
+
+}
