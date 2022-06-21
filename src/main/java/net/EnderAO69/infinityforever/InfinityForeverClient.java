dfbac2555e365d929739839db6f9a759df5c4c4d
EnderAO69 <alexanderotoole@icloud.com>
2022-06-21 07:08:40 +0100

    First Commit

---
+++ src/main/java/net/EnderAO69/infinityforever/InfinityForeverClient.java
@@ -0,0 +1,10 @@
+package net.EnderAO69.infinityforever;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InfinityForeverClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
